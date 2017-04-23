985f0e5981238d17cf16c9a9dbbe87ff8042e0a3
MSI\SoulExorcist <aragornbarbossa@gmail.com>
2017-04-23 16:47:49 -0400

    added comments

--- List.java
+++ List.java
@@ -1,6 +1,6 @@
 public class List
 {
-  public static int[] bubbleSort(int[] x)
+  public static int[] bubbleSort(int[] x) // Sorts the array from smallest to largest
   {
     int i, j, temp = 0;
     for(i = 0; i < x.length - 1; i++)
@@ -18,17 +18,17 @@ public class List
     return x;
   }
   
-  public static void main(String[] args)
+  public static void main(String[] args) // main method
   {
-    int[] x = {1, 23, 5, 67, 6, 81, 9, 102, 30, 52, 8};
+    int[] x = {1, 23, 5, 67, 6, 81, 9, 102, 30, 52, 8}; // array to be sorted
     System.out.println("Unsorted List:");
-    for(int y = 0; y < x.length; y++)
+    for(int y = 0; y < x.length; y++) // prints unsorted array
     {
       System.out.println(x[y]);
     }
     System.out.println("\nSorted List:");
     x = bubbleSort(x);
-    for(int z = 0; z < x.length; z++)
+    for(int z = 0; z < x.length; z++) // prints sorted array
     {
       System.out.println(x[z]);
     }
