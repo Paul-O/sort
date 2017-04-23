5a112d22f62d15b0d7d4926d29acd1e8c6219303
MSI\SoulExorcist <aragornbarbossa@gmail.com>
2017-04-23 16:37:35 -0400

    made bubbleSort method

--- List.java
+++ List.java
@@ -1,4 +1,21 @@
 public class List
 {
-  int[] x = {1, 23, 5, 67, 6, 81, 9, 102, 30, 52, 8}
+  int[] x = {1, 23, 5, 67, 6, 81, 9, 102, 30, 52, 8};
+  public int[] bubbleSort(int[] x)
+  {
+    int i, j, temp = 0;
+    for(i = 0; i < x.length - 1; i++)
+    {
+      for(j = 0; j < x.length - 1 - i; j++)
+      {
+        if(x[j] > x[j + 1])
+        {
+          temp = x[j];
+          x[j] = x[j + 1];
+          x[j + 1] = temp;
+        }
+      }
+    }
+    return x;
+  }
 }
